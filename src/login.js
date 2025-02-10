c5c100c003ec410dcebbc63f1851fb4211f675bb
zahia0204 <zahya09kouhoub@gmail.com>
2025-02-10 19:30:19 +0100

    added login

---
+++ src/login.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import './login.css';
+import logo from './logo.jpg';
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Username:', username);
+    console.log('Password:', password);
+  };
+
+  return (
+    <div>
+      <div className="wrapper">
+        <form onSubmit={handleSubmit}>
+          <div>
+            <img src={logo} alt="Login" className="login-image" />
+          </div>
+          <div className="input-box">
+            <input
+              type="text"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div className="input-box">
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
