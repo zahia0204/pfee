17d20f50864b20c10ca296ef3d6b02557f9e5a2c
zahia0204 <zahya09kouhoub@gmail.com>
2025-02-13 18:38:54 +0100

    added dashboard route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
 import Login from "./login";
 import Home from "./home";
+import Dashboard from "./Dashboard";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
         </Routes>
       </div>
     </Router>
---
+++ src/Dashboard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "./navBar";
+import Sidebar from "./sideBar";
+
+
+const Dashboard = () => {
+
+    return ( 
+    <div> 
+        <Navbar/>
+        <Sidebar/>
+            <h1>Dashboard</h1>
+    </div>
+     );
+}
+ 
+export default Dashboard;
